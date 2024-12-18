4fff2e160593400bf4a1927b93b0564d3691a5a9
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-12-18 18:13:04 -0300

    feat: cria a tabela do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -6,7 +6,12 @@ import { AuthContext } from '../../contexts/auth';
 
 import Header from '../../componentes/Header';
 import Title from '../../componentes/Title';
-import { FiHome } from 'react-icons/fi';
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi';
+
+import './dashboard.css';
+
+// importar o link para navegarmos entre as páginas
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
     const { logout } = useContext(AuthContext);
@@ -17,14 +22,49 @@ export default function Dashboard() {
     return (
         <div>
             <Header/>
-            <div className='content'>
-                <div>
-                    <Title name='Home'>
-                        <FiHome size={25}/>
-                        
-                    </Title>
-                </div>
+            <div className='content'>       
+                <Title name='Tickets'>
+                    <FiMessageSquare size={25}/>                   
+                </Title>  
                 
+            {/*Abrir um fragment, que é como se fosse uma div  */}
+            {/* é uma tag sem estilização */}
+
+            <>
+                <Link to="/new" className='new'>
+                    <FiPlus color='#fff' size={25}/>
+                    Novo chamado               
+                </Link>
+                <table>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Cliente</th>
+                            <th scope='col'>Assunto</th>
+                            <th scope='col'>Status</th>
+                            <th scope='col'>Cadastrado em</th>
+                            <th scope='col'>#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td data-label="Cliente">Mercado Esquina</td>
+                            <td data-label="Assunto">Suporte</td>
+                            <td data-label="Assunto">Em aberto</td>
+                            <td data-label="Assunto">12/05/2022</td>
+                            <td>
+                                <button className='action' style={{ backgroundColor: '#3583f6'}}>
+                                    <FiSearch color='#fff' size={17}/>
+                                </button>
+                                <button className='action' style={{ backgroundColor: '#f6a935'}}>
+                                    <FiEdit2 color='#fff' size={17}/>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            
+            
+            </>
             </div>
             
             <button onClick={handleLogout}>Sair da conta</button>
